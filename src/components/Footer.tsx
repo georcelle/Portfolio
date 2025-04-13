b3b3a0c2f45ac0ebca568da70754645535f6e716
Georcelle <2234664@slu.edu.ph>
2025-04-13 23:04:35 +0800

    + added personal touch to the footer adding m y favorite quote

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/georcelle" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/georcellenuarin/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>The only permanent thing in this world is change.<a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer"></a></p>
     </footer>
   );
 }
