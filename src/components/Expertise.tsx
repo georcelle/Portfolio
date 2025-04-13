0c838e4d6e67a7698bfb6d677585dd9260fd63bb
Georcelle <2234664@slu.edu.ph>
2025-04-13 23:04:17 +0800

    + fixed the expertise and tailored to my personal expertise

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,44 +1,38 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe, faLaptopCode, faPalette } from '@fortawesome/free-solid-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
 const labelsFirst = [
     "React",
-    "TypeScript",
     "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
+    "HTML",
+    "CSS",
     "Python",
-    "SQL",
-    "PostgreSQL",
-    "Postman"
+    "JavaFX",
+    "Java",
+    "Git",
 ];
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    "Figma",
+    "Canva",
+    "Miro",
+    "User Testing",
+    "Wireframing",
+    "Prototyping",
+    "Excalidraw",
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "Git",
+    "Java",
+    "Python",
+    "HTML",
+    "CSS",
+    "PROLOG",
 ];
 
 function Expertise() {
@@ -48,9 +42,9 @@ function Expertise() {
             <h1>Expertise</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
+                    <FontAwesomeIcon icon={faLaptopCode} size="3x"/>
+                    <h3>FrontEnd Development</h3>
+                    <p>I specialize in building modern, responsive, and user-friendly interfaces using technologies like React, JavaScript, and CSS. My strengths lie in translating design concepts into clean, functional front-end code, ensuring great performance across devices. I am highly familiar with the frontend workflow, version control, and collaborating with designers to bring UI visions to life.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsFirst.map((label, index) => (
@@ -60,9 +54,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                <FontAwesomeIcon icon={faPalette} size="3x" />                   
+                 <h3>UI/UX Design</h3>
+                    <p>I focus on creating intuitive and user-friendly interfaces through thorough wireframing, prototyping, and user testing. Using tools like Figma, Canva, and Miro, I translate ideas into seamless digital experiences. I’m experienced in gathering user feedback, refining design flows, and collaborating across teams to enhance usability and accessibility.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsSecond.map((label, index) => (
@@ -72,9 +66,9 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <FontAwesomeIcon icon={faGlobe} size="3x"/>
+                    <h3>Web Development</h3>
+                    <p>I build full-stack applications and smart web solutions by combining modern development practices with emerging AI technologies. My work includes creating scalable backend systems and integrating GenAI to enable advanced features like predictive analytics and intelligent automation, enhancing user experience and business efficiency.</p>
                     <div className="flex-chips">
                         <span className="chip-title">Tech stack:</span>
                         {labelsThird.map((label, index) => (
